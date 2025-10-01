e73f253820eb311eba8d245cd3477fe377799125
lindo-semetsi-hub <lindosemetsi@gmail.com>
2025-10-01 14:26:34 +0200

    commit errorhandler

---
+++ src/middleware/errorHandler.ts
@@ -0,0 +1,13 @@
+import { Request, Response, NextFunction } from "express";
+
+export function errorHandler(err: any, req: Request, res: Response, next: NextFunction) {
+    console.error("Error:", err);
+    if (res.headersSent) {
+        return next (err);
+    }
+
+    if (err.status && err.message) {
+        return res.status(err.status).json({ error: err.message});
+    } 
+    res.status(500).json({ error: "Internal Server Error"});
+}
